0ea109c4ecf05edb57c6d95f98d449d681d7e79e
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-10-05 17:28:58 -0700

    semi-fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,32 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+	let screenType;
+	onMount(async () => {
+		const ua = navigator.userAgent;
+		if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+			// tablet
+			screenType = 1;
+		} else if (
+			/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+				ua
+			)
+		) {
+			// phone
+			screenType = 2;
+		} else {
+			//laptop
+			screenType = 3;
+		}
+	});
 </script>
 
-<slot />
-<div id="phoneBlock"><p class="sml">wip, use desktop</p></div>
+{#if screenType == 1 || screenType == 2}
+	<div id="phoneBlock"><p class="sml">wip, use desktop</p></div>
+{:else}
+	<slot />
+{/if}
 
 <style>
 	@media only screen and (max-width: 768px) {
