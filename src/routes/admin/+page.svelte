7f0db230c76facfe98cc4dd3c571a8413bcb97b3
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-02 20:01:40 -0400

    restructure

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,7 @@
 <body>
 	<!-- Include the script that builds the page and powers Netlify CMS -->
 	<script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-	<link href="/src/routes/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+	<link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
 </body>
 
 <svelte:head>
