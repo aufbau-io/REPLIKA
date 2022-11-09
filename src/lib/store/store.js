f2d8c659dfd4b9c6860dc498092efc7f03a2cd88
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-08 19:13:35 -0500

    default to desktop screen

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -2,7 +2,7 @@ import { writable, derived } from 'svelte/store';
 
 export const animations = writable(true);
 export const absoluteIndex = writable(0);
-export const screenType = writable(null);
+export const screenType = writable(3);
 
 // javascript modulo doesn't work on negative numbers
 function mod(n, m) {
