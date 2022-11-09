44972f1b26f0cc75fe8aa880aa100da3d071b0cb
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-08 19:37:01 -0500

    FIX ERROR

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,8 @@
 <body>
+	<!-- THIS TAG SHOULD DEFINE A CUSTOM CONFIG ROUTE BUT IT BREAKS THE IDENTITY WIDGET -->
+	<!--link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" /-->
+
 	<!-- Include the script that builds the page and powers Netlify CMS -->
-	<link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
 	<script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
 </body>
 
