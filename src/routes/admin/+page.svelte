7dad455d0f77b7637a443c03dd1208b493838343
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-09 12:51:00 -0500

    update

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,6 @@
 <body>
-	<!-- THIS TAG SHOULD DEFINE A CUSTOM CONFIG ROUTE BUT IT BREAKS THE IDENTITY WIDGET -->
 	<link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
-
+	<title>CONTENT MANAGER</title>
 	<!-- Include the script that builds the page and powers Netlify CMS -->
 	<script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
 </body>
